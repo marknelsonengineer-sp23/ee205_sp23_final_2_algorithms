53465b6c80c081eb051f03731b26978f649d6a76
Mark Nelson <marknels@hawaii.edu>
2023-05-09 16:34:40 -1000

    Get main.cpp to be Doxygen compliant and burn down a few lint issues

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 //         University of Hawaii, College of Engineering
 //         EE 205 - Spr 2023 - Final Exam 2 - Algorithms
 //
+/// Your program must iterate-and-test to compute the result.
+/// You can not use numerical methods.
+///
+/// Your program has to solve this eventually, but it does not have to produce a
+/// result during the final.  It takes my computer 10 minutes to work through
+/// the entire set, so manage your time well.
+///
+/// The program must print exactly one number at the end, so before you submit,
+/// disable all debugging output as that will confuse the autograder.
+///
 /// @file   main.cpp
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -9,23 +19,17 @@
 
 using namespace std;
 
-/// Your program must iterate-and-test to compute the result.
-/// You can not use numerical methods.
-
-/// Your program has to solve this eventually, but it does not have to produce a
-/// result during the final.  It takes my computer 10 minutes to work through
-/// the entire set, so manage your time well.
-
-/// The program must print exactly one number at the end, so before you submit,
-/// disable all debugging output as that will confuse the autograder.
-
-#define INT long
-#define INT_FORMAT "%010ld"
+#define INT        long      ///< The signed datatype we need to get to `1,000,000,000`
+#define INT_FORMAT "%010ld"  ///< The format for printing #INT
 
 //                  0123456789
-#define START_VALUE 0100000000
-#define STOP_VALUE  9999999999
+#define START_VALUE 0100000000  ///< Start searching from here (inclusive)
+#define STOP_VALUE  9999999999  ///< End at this number (inclusive)
 
+/// Test `i` and determine if each digit is unique.
+///
+/// @param i The number to test
+/// @return `true` if each digit is unique.  `false` if there is one or more duplicate digits.
 bool test( INT i ) {
    char szInt[11];
    sprintf( szInt, INT_FORMAT, i );
@@ -39,18 +43,23 @@ bool test( INT i ) {
    }
 
    for( int j = 0 ; j < 10 ; j++ ) {
-      int k = szInt[j] - '0';
+      const int k = szInt[j] - '0';
       if( hasInt[k] ) {
          return false;
-      } else {
-         hasInt[k] = true;
       }
+      hasInt[k] = true;
    }
 
    return true;
 }
 
 
+/// The entry point for this program
+///
+/// Iterate over #START_VALUE : #STOP_VALUE and count the number of digits
+/// that have unique values.
+///
+/// @return The result code
 int main() {
    INT count = 0 ;
 
